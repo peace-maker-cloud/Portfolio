5e55f9d500a592036fa58942c8bf584f92de6797
Vigneshwaran Ravi <110144091+peace-maker-cloud@users.noreply.github.com>
2023-12-25 21:10:40 +0530

    done

--- src/components/visuals/Visualizer.jsx
+++ src/components/visuals/Visualizer.jsx
@@ -8,19 +8,25 @@ import { Contact } from "./division/Contact";
 export const Visualizer = () => {
   return (
     <div className="w-[96vw] h-full hide-scrollbar overflow-y-scroll snap-y snap-proximity transition-all scroll-smooth  bg-black text-white">
-      <section id="HOME" className="w-full h-screen snap-start">
+      <section id="HOME" className="w-full h-screen snap-start home-anime">
         <Home />
       </section>
-      <section id="PROJECT" className="w-full h-screen snap-start">
+      <section
+        id="PROJECT"
+        className="w-full h-screen snap-start project-anime"
+      >
         <Projects />
       </section>
-      <section id="EDUCAT" className="w-full h-screen snap-start">
+      <section id="EDUCAT" className="w-full h-screen snap-start educat-anime">
         <Educat />
       </section>
-      <section id="EXTRAS" className="w-full h-screen snap-start">
+      <section id="EXTRAS" className="w-full h-screen snap-start extras-anime">
         <Extras />
       </section>
-      <section id="CONTACT" className="w-full h-screen snap-start">
+      <section
+        id="CONTACT"
+        className="w-full h-screen snap-start contact-anime"
+      >
         <Contact />
       </section>
     </div>
