b876845e16d19fa7f83283d46891bd3902bc4bdf
Vigneshwaran Ravi <110144091+peace-maker-cloud@users.noreply.github.com>
2023-12-26 20:06:10 +0530

    DOne

--- src/components/visuals/division/Home.jsx
+++ src/components/visuals/division/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 import dp from "../../../assets/Images/profile pic/rvdp.png";
+import lWing from "../../../assets/Images/wings/left wing.png";
+import rWing from "../../../assets/Images/wings/right wing.png";
 
 export const Home = ({ actvSec }) => {
   const colorList = [
@@ -12,29 +14,41 @@ export const Home = ({ actvSec }) => {
     "pink",
     "yellow",
   ];
-  const [tColor, setTColor] = useState(() => {
-    const randomIndex = Math.floor(Math.random() * colorList.length);
-    return colorList[randomIndex];
-  });
+  const tColor = colorList[Math.floor(Math.random() * colorList.length)];
+
   if (actvSec === "HOME") {
   }
   return (
     <>
-      <div className="hover:home-anime">
+      <div>
         <div className="introduction flex justify-evenly py-44">
           <div className="about me flex flex-col justify-center gap-5">
-            <h1 className=" text-orange-400 text-5xl font-bold">
-              My name is <span className="text-7xl">Vigneshwaran Ravi</span>
+            <h1 className="text-orange-400 text-4xl font-bold">
+              My name is <span className="text-6xl">Vigneshwaran Ravi</span>
             </h1>
-            <p className="text-4xl font-bold">
+            <p className="text-3xl font-bold">
               I'm a{" "}
-              <span className="text-5xl" style={{ color: tColor }}>
+              <span className="text-4xl" style={{ color: tColor }}>
                 FrontEnd Web Developer
               </span>
             </p>
           </div>
-          <div className="profile pic relative">
-            <img src={dp} alt="DP" />
+          <div
+            className={`${
+              actvSec === "HOME" ? "profile-pic" : ""
+            } relative transition-all`}
+          >
+            <img
+              className="left-wing absolute bottom-24 left-32"
+              src={lWing}
+              alt="Left Wing"
+            />
+            <img className="dp z-50 relative" src={dp} alt="DP" />
+            <img
+              className="right-wing absolute bottom-24 right-32"
+              src={rWing}
+              alt="Right Wing"
+            />
           </div>
         </div>
       </div>
